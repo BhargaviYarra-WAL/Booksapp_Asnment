0cb90fa55cee4416a6d27f3f1d9f42da3bfbd22b
BhargaviYarra-WAL <96226656+BhargaviYarra-WAL@users.noreply.github.com>
2022-05-24 09:26:07 +0530

    Delete booksapp_final directory

--- booksapp_final/backend/routes/bookuser.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getusers, userbyid, loginUser } = require("../controllers/bookuser");
-const router = express.Router();
-router.get("/", getusers);
-router.get("/:id", userbyid);
-router.post("/checklogin", loginUser);
-
-module.exports = router;
--- booksapp_final/frontend/src/BooksContext.jsx
+++
@@ -1,4 +0,0 @@
-const { createContext } = require('react');
-
-const BooksContext = createContext({});
-export default BooksContext;
